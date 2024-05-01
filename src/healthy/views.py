00ca5bc2e5f035210c6be167917b9d6844cf2eb7
Christian Hartung <christian.hartung@olist.com>
2024-05-01 13:50:07 -0300

    chore: add ruff formatter

--- src/healthy/views.py
+++ src/healthy/views.py
@@ -2,18 +2,19 @@
 #
 # SPDX-License-Identifier: MIT
 from http import HTTPStatus
+from typing import ClassVar
 
 from django.http import HttpRequest, HttpResponse
 from django.views import View
 
 
 class PingView(View):
-    http_method_names = [
+    http_method_names: ClassVar = [
         "get",
         "head",
         "options",
         "trace",
     ]
 
-    async def get(self, request: HttpRequest) -> HttpResponse:
+    async def get(self, request: HttpRequest) -> HttpResponse:  # noqa: ARG002
         return HttpResponse("Pong", status=HTTPStatus.OK)
--- tests/test_views.py
+++ tests/test_views.py
@@ -22,14 +22,14 @@ def test_methods_not_allowed(method, client):
     assert response.status_code == HTTPStatus.METHOD_NOT_ALLOWED
 
 
-@pytest.mark.asyncio()
+@pytest.mark.asyncio
 async def test_get_ping_async(async_client):
     response = await async_client.get(reverse("healthy:ping"))
 
     assert response.status_code == HTTPStatus.OK
 
 
-@pytest.mark.asyncio()
+@pytest.mark.asyncio
 @pytest.mark.parametrize("method", ["post", "put", "patch", "delete"])
 async def test_methods_not_allowed_async(method, async_client):
     django_client_method = getattr(async_client, method)
